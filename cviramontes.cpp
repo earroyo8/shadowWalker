a2ad4efe00b50382978ebecc6d3b125b651f7bd2
Christian V <123493021+ViramC420@users.noreply.github.com>
2023-04-10 09:33:39 -0700

    fixed indenting

--- cviramontes.cpp
+++ cviramontes.cpp
@@ -5,11 +5,16 @@
 
 
 //enemy & player are placeholder for desired game character names
-bool playerCollision(float x1, float y1, float width1, float height1, float x2, float y2, float width2, float height2) {
-    return (x1 < x2 + width2 && x1 + width1 > x2 && y1 < y2 + height2 && y1 + height1 > y2);
+bool playerCollision (float x1, float y1, float width1, 
+		      float height1, float x2, float y2, float width2, float height2) 
+{
+    return (x1 < x2 + width2 && x1 + width1 > x2 && 
+	    y1 < y2 + height2 && y1 + height1 > y2);
 }
 
-void moveEnemyToPlayer(float enemyX, float enemyY, float playerX, float playerY, float deltaTime) { 
+void moveEnemyToPlayer (float enemyX, float enemyY, 
+			float playerX, float playerY, float deltaTime) 
+{ 
     float speed = 0.1f; //adjust for desired speed
     float dx = playerX - enemyX;
     float dy = playerY - enemyY;
@@ -21,7 +26,8 @@ void moveEnemyToPlayer(float enemyX, float enemyY, float playerX, float playerY,
 	float movY = speed * dirY * deltaTime;
 	float nextX = enemyX + movX;
 	float nextY = enemyY + movY;
-	if (!playerCollision(nextX, nextY, enemyWidth, enemyHeight, playerX, playerY, playerWidth, playerHeight)) {
+	if (!playerCollision(nextX, nextY, enemyWidth, enemyHeight, 
+			     playerX, playerY, playerWidth, playerHeight)) {
 	    enemyX = nextX;
 	    enemyY = nextY;
 	}
