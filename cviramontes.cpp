3a30c4cc91fee6caea819f1dba6e2ee6f1b92652
Christian V <123493021+ViramC420@users.noreply.github.com>
2023-04-26 10:52:16 -0700

    scratched everything and added cone

--- cviramontes.cpp
+++ cviramontes.cpp
@@ -1,35 +1,16 @@
-//enemy pathing to player
-#include <vector>
-#include <csdtlib>
-#include <ctime>
+#include <iostream>
+#include <cmath>
+#include <GL/glx.h>
 
-
-//enemy & player are placeholder for desired game character names
-bool playerCollision (float x1, float y1, float width1, 
-		      float height1, float x2, float y2, float width2, float height2) 
-{
-    return (x1 < x2 + width2 && x1 + width1 > x2 && 
-	    y1 < y2 + height2 && y1 + height1 > y2);
-}
-
-void moveEnemyToPlayer (float enemyX, float enemyY, 
-			float playerX, float playerY, float deltaTime) 
-{ 
-    float speed = 0.1f; //adjust for desired speed
-    float dx = playerX - enemyX;
-    float dy = playerY - enemyY;
-    float distance = sqrt((dx * dx) + (dy * dy));
-    if (distance > 0.0f) {
-	float dirX = dx / distance;
-	float dirY = dy / distance;
-	float movX = speed * dirX * deltaTime;
-	float movY = speed * dirY * deltaTime;
-	float nextX = enemyX + movX;
-	float nextY = enemyY + movY;
-	if (!playerCollision(nextX, nextY, enemyWidth, enemyHeight, 
-			     playerX, playerY, playerWidth, playerHeight)) {
-	    enemyX = nextX;
-	    enemyY = nextY;
-	}
+void initguardCone(float angle, float distance) {
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2f(0.0f, 0.0f); //center of cone
+    glColor3f(1.0f, 0.0f, 1.0f); //cone to purple
+    for (float i = -angle / 2; i <= angle / 2; i += 1.0f) {
+        float rad = i * (M_PI / 180.0f);
+        float x = distance * cos(rad);
+        float y = distance * sin(rad);
+        glVertex2f(x, y);
     }
+    glEnd();
 }
