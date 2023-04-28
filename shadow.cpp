a836a1281457d65a9fb965a8f1a407e80a3b1e59
Christian V <123493021+ViramC420@users.noreply.github.com>
2023-04-28 11:23:50 -0700

    additional 9 guards on board with random spawns

--- shadow.cpp
+++ shadow.cpp
@@ -73,7 +73,7 @@ using namespace std;
 #define SPY_CONE_DISTANCE 25.0f
 #define SPY_ANGLE_SNEAK 60.0f
 #define SPY_DISTANCE_SNEAK 5.0f
-#define MAX_GUARDS 10
+const int MAX_GUARDS = 10;
 
 //
 
@@ -95,6 +95,7 @@ typedef struct t_spy {
 typedef struct t_guard {
     int status;
     int pos[2];
+    int direction;
 } Guard;
 //
 //
@@ -506,20 +507,26 @@ void initSpy()
     //spy.timer = glfwGetTime() + 0.5;
 }
 
+#define grand() (rand() % 3 - 1)
 void initGuard()
 {
-   int i=rand() % MAX_SIZE;
-   int j=rand() % MAX_SIZE;
-    
-   while (walls[i][j]!=0) {
-    i=rand() % MAX_SIZE;
-    j=rand() % MAX_SIZE;
-   }
-    
-   g.guard.status = 1;
-   g.guard.pos[0] = j;
-   g.guard.pos[1] = i;
-} 
+    for (int k=0; k<MAX_GUARDS; ++k) {
+        int i=rand() % MAX_SIZE;
+        int j=rand() % MAX_SIZE;
+
+        while (walls[i][j]!=0) {
+            i=rand() % MAX_SIZE;
+            j=rand() % MAX_SIZE;
+        }
+
+        g.guard[k].status = 1;
+        g.guard[k].pos[0] = j;
+        g.guard[k].pos[1] = i;
+
+        g.guard[k].direction = DIRECTION_RIGHT;
+        //g.guard[k].pos[1] += 1;
+    }
+}
 
 void init()
 {
@@ -824,31 +831,51 @@ void physics(void)
                 g.spy.pos[0][0]=headpos[0];
                 g.spy.pos[0][1]=headpos[1];
             }
+            for (int z=0; z<MAX_GUARDS; ++z) {
+                if (guardHit(headpos,g.guard[z].pos[0],g.guard[z].pos[1]))
+                    resetGame();
 
-            if (guardHit(headpos,g.guard.pos[0],g.guard.pos[1]))
-                resetGame();
+                return;
+                //new position for guard...
+                int collision=0;
+                int ntries=0;
 
-            return;
-            //new position for guard...
-            int collision=0;
-            int ntries=0;
-            while (1) {
-                g.guard.pos[0] = rand() % g.gridDim;
-                g.guard.pos[1] = rand() % g.gridDim;
-                collision=0;
-                for (i=0; i<g.spy.length; i++) {
-                    if (g.guard.pos[0] == g.spy.pos[i][0] &&
-                            g.guard.pos[1] == g.spy.pos[i][1]) {
-                        collision=1;
+                switch (g.guard[z].direction) {
+                    case DIRECTION_DOWN:
+                        g.guard[z].pos[1] += 1;
                         break;
-                    }
+                    case DIRECTION_LEFT:
+                        g.guard[z].pos[0] -= 1;
+                        break;
+                    case DIRECTION_UP:
+                        g.guard[z].pos[1] -= 1;
+                        break;
+                    case DIRECTION_RIGHT:
+                        g.guard[z].pos[0] += 1;
+                        break;
+                    case NO_MOVEMENT:
+                        g.guard[z].pos[0]+=0;
+                        g.guard[z].pos[1]+=0 ;
+                        break;
                 }
-                if (!collision) break;
-                if (++ntries > 1000000) break;
-            }
-            Log("new guard: %i %i\n",g.guard.pos[0],g.guard.pos[1]);
-            return;
 
+                while (1) {
+                    g.guard[z].pos[0] = rand() % g.gridDim;
+                    g.guard[z].pos[1] = rand() % g.gridDim;
+                    collision=0;
+                    for (i=0; i<g.spy.length; i++) {
+                        if (g.guard[z].pos[0] == g.spy.pos[i][0] &&
+                                g.guard[z].pos[1] == g.spy.pos[i][1]) {
+                            collision=1;
+                            break;
+                        }
+                    }
+                    if (!collision) break;
+                    if (++ntries > 1000000) break;
+                }
+                Log("new guard: %i %i\n",g.guard[z].pos[0],g.guard[z].pos[1]);
+                return;
+            }
 }
 
 
@@ -995,16 +1022,18 @@ void render(void)
     //
     //
     //draw guard...
-    getGridCenter(g.guard.pos[1],g.guard.pos[0],cent);
-    initguardCone(CONE_ANGLE, CONE_DISTANCE);
-    //glTranslatef(g.guard.pos[1],g.guard.pos[0],0.0f);
-    glColor3f(1.0, 0.1f, 0.0f);
-    glBegin(GL_QUADS);
-    glVertex2i(cent[0]-5, cent[1]-5);
-    glVertex2i(cent[0]-5, cent[1]+5);
-    glVertex2i(cent[0]+5, cent[1]+5);
-    glVertex2i(cent[0]+5, cent[1]-5);
-    glEnd();
+    for (int k=0; k<MAX_GUARDS; ++k) {
+        getGridCenter(g.guard[k].pos[1],g.guard[k].pos[0],cent);
+        initguardCone(CONE_ANGLE, CONE_DISTANCE);
+        //glTranslatef(g.guard[k].pos[1],g.guard[k].pos[0],0.0f);
+        glColor3f(1.0, 0.1f, 0.0f);
+        glBegin(GL_POLYGON);
+        glVertex2i(cent[0]-5, cent[1]-5);
+        glVertex2i(cent[0]-5, cent[1]+5);
+        glVertex2i(cent[0]+5, cent[1]+5);
+        glVertex2i(cent[0]+5, cent[1]-5);
+        glEnd();
+    }
     //
     //
     r.left   = g.xres/2;
