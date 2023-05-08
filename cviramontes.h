dfb39d9bfff612f9c5aa2c9f0b6cf2e0c30ae592
Christian V <123493021+ViramC420@users.noreply.github.com>
2023-05-08 11:26:46 -0700

    Update cviramontes.h

--- cviramontes.h
+++ cviramontes.h
@@ -1,6 +1,14 @@
 #ifndef _CVIRAMONTES_H_
 #define _CVIRAMONTES_H_
 
+//def and const
+#define DIRECTION_DOWN 0
+#define DIRECTION_LEFT 1
+#define DIRECTION_UP 2
+#define DIRECTION_RIGHT  3
+#define NO_MOVEMENT 4
+
+extern void updateGuardPos(int& direction, int& x, int& y);
 extern void initguardCone(float angle, float distance);
 extern void incrementGuard(int& enemyCount);
 
